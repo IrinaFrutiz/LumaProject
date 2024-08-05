a9d1c90334198afdd7589fc07c4c915a02f8183b
irina <irinafrutiz@gmail.com>
2024-08-05 14:54:05 +0200

    test ci

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -21,10 +21,6 @@ class TestMainPage(BaseTest):
         assert self.main_page.check_url_is_(MAIN_PAGE_URL), \
             f"Current URL is not equal {MAIN_PAGE_URL}"
 
-    def test_test_ci(self):
-        assert 2 == 2, \
-            "Something went wrong"
-
 
 class TestLoggedUserMainPage(BaseTest):
     @pytest.fixture(scope="function")
