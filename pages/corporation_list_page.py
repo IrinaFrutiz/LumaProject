f198e586736276d810abfaa4f231877aa34b7fcd
IrinaFrutiz <34806511+IrinaFrutiz@users.noreply.github.com>
2024-11-04 17:06:35 +0100

    change: (#32)
    *check_number_of_products_on_page -> check_number_of_products_on_page_is_ now return True or False
    *check_all_visibility_ add try/except
    *check_message_that_product_added_to_the_comparison_list add check that page is load
    *delete xfail mark from test_adding_the_product_to_the_comparison_list
    
    add:
    TestProductPage add test_adding_the_product_to_the_comparison_list

--- pages/corporation_list_page.py
+++ pages/corporation_list_page.py
@@ -1,4 +1,5 @@
 import allure
+from selenium.common import TimeoutException
 
 from base.base_page import BasePage
 from data.links import CORPORATION_LIST_URL
@@ -6,19 +7,24 @@ from data.links import CORPORATION_LIST_URL
 ALL_PRODUCTS = ('xpath', '//td[@data-th="Product"]')
 ALL_REMOVE_PRODUCT = ('css selector', '.action.delete')
 ACTION_OK = ('css selector', '.action-accept')
+MSG_NO_ITEM = ('css selector', '.message.empty')
 
 
 class CorporationListPage(BasePage):
     URL = CORPORATION_LIST_URL
 
     @allure.step("Return Number of products on the corporation list page")
-    def check_number_of_products_on_page(self):
-        return len(self.find_all(ALL_PRODUCTS))
+    def check_number_of_products_on_page_is_(self, number):
+        return len(self.find_all(ALL_PRODUCTS)) == number
 
     @allure.step("Delete all products from Corporation List")
     def remove_products(self):
-        self.check_all_visibility_(ALL_REMOVE_PRODUCT)
-        for button in self.find_all(ALL_REMOVE_PRODUCT):
-            button.click()
-            self.click_button(ACTION_OK)
-            self.check_page_loaded()
+        try:
+            self.check_element_not_visible_(MSG_NO_ITEM)
+            for button in self.find_all(ALL_REMOVE_PRODUCT):
+                button.click()
+                self.click_button(ACTION_OK)
+                self.check_page_loaded()
+        except TimeoutException:
+            pass
+        self.check_element_visibility_(MSG_NO_ITEM)
